39a15187c6bb0493cc9a75cd59ab21e08c532509
Nivesh26 <nivesh.shrestha.94@gmail.com>
2026-01-28 20:57:09 +0545

    Update PageTransition component to reduce animation duration for smoother transitions

--- frontend/src/Components/PageTransition.tsx
+++ frontend/src/Components/PageTransition.tsx
@@ -10,8 +10,7 @@ const PageTransition = ({ children }: PageTransitionProps) => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.12 }}
     >
       {children}
     </motion.div>
