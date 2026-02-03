5e0aa7d3654e5539650ac7639092bc7052f8d6e0
Nivesh26 <nivesh.shrestha.94@gmail.com>
2026-02-03 22:03:35 +0545

    Update Topbar component text to remove "Made in Nepal" for a more concise message

--- frontend/src/Components/Topbar.tsx
+++ frontend/src/Components/Topbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Topbar = () => {
   return (
        <div className="bg-gray-100 text-black text-sm text-center py-1">
-           Bringing Nepali Traditions Online Made in Nepal
+           Bringing Nepali Traditions Online.
       </div>
   )
 }
